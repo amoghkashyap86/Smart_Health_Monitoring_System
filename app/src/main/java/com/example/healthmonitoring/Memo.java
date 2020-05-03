846839dcd7744a75057d486b37da3bf0d6c77e22
amoghkashyap86 <amogh87kashyap@gmail.com>
2020-05-03 17:49:08 +0530

    update_1.0

--- app/src/main/java/com/example/healthmonitoring/Memo.java
+++ app/src/main/java/com/example/healthmonitoring/Memo.java
@@ -9,6 +9,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -21,54 +22,54 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Memo extends AppCompatActivity {
-    static ArrayList<String> notes=new ArrayList<>();
+    static ArrayList<String> notes = new ArrayList<String>();
     static ArrayAdapter arrayAdapter;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater=getMenuInflater();
-        menuInflater.inflate(R.menu.menu,menu);
-        return super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()==R.id.newNotes){
-            Intent intent=new Intent(getApplicationContext(),Notes.class);
+        if (item.getItemId() == R.id.newmemo) {
+            Intent intent = new Intent(getApplicationContext(), Notes.class);
             startActivity(intent);
             return true;
         }
+
         return false;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_memo);
-        ListView listView=(ListView) findViewById(R.id.list);
-        SharedPreferences sharedPreferences=getApplicationContext().getSharedPreferences(" com.example.healthmonitoring", Context.MODE_PRIVATE);
-        HashSet<String> set=(HashSet<String>) sharedPreferences.getStringSet("notes",null);
-        if(set==null) {
-            notes.add("1st note");
-        }else {
-            notes=new ArrayList(set);
+        setContentView(R.layout.activity_memo2);
+        ListView listView = (ListView) findViewById(R.id.List);
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("", Context.MODE_PRIVATE);
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes", null);
+        if (set == null) {
+            notes.add("click here to add medical data");
+        } else {
+            notes = new ArrayList(set);
         }
-        arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,notes);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
         listView.setAdapter(arrayAdapter);
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent=new Intent(getApplicationContext(),Notes.class);
-                intent.putExtra("notesId",i);
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(), Notes.class);
+                intent.putExtra("noteId", i);
                 startActivity(intent);
-                return true;
-
             }
         });
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(final AdapterView<?> adapterView, View view, int i, long l) {
-                final int itemToDelete=i;
+                final int itemToDelete = i;
                 new AlertDialog.Builder(Memo.this)
                         .setIcon(android.R.drawable.ic_dialog_alert)
                         .setTitle("are u sure")
@@ -76,17 +77,18 @@ public class Memo extends AppCompatActivity {
                         .setPositiveButton("yes", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
-                                notes.remove(itemToDelete );
+                                notes.remove(itemToDelete);
                                 arrayAdapter.notifyDataSetChanged();
-                                SharedPreferences sharedPreferences=getApplicationContext().getSharedPreferences(" com.example.memo", Context.MODE_PRIVATE);
-                                HashSet<String> set =new HashSet<>(Memo.notes);
-                                sharedPreferences.edit().putStringSet("notes",set).apply();
+                                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(" com.example.memo", Context.MODE_PRIVATE);
+                                HashSet<String> set = new HashSet<>(Memo.notes);
+                                sharedPreferences.edit().putStringSet("notes", set).apply();
+
                             }
                         })
-                        .setNegativeButton("no!",null)
+                        .setNegativeButton("no", null)
                         .show();
-
                 return true;
+
             }
         });
 
--- app/src/main/java/com/example/healthmonitoring/Notes.java
+++ app/src/main/java/com/example/healthmonitoring/Notes.java
@@ -9,51 +9,53 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.HashSet;
 
 public class Notes extends AppCompatActivity {
     int noteID;
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notes);
-        EditText editText=findViewById(R.id.editText);
+        setContentView(R.layout.activity_notes2);
+        EditText editText=findViewById(R.id.editText4);
         Intent intent=getIntent();
         noteID=intent.getIntExtra("noteId",-1);
-        if(noteID!=-1){
-            editText.setText(Memo.notes.get(noteID));
+         if(noteID!=-1){
+             editText.setText(Memo.notes.get(noteID));
+         }else {
+             Memo.notes.add("");
+             noteID=Memo.notes.size()-1;
+             Memo.arrayAdapter.notifyDataSetChanged();
+         }
+         editText.addTextChangedListener(new TextWatcher() {
+             @Override
+             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
 
-        }else {
-            Memo.notes.add("");
-            noteID=Memo.notes.size()-1;
-            Memo.arrayAdapter.notifyDataSetChanged();
-        }
+             }
 
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+             @Override
+             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-            }
+                     Memo.notes.set(noteID, String.valueOf(charSequence));
+                     Memo.arrayAdapter.notifyDataSetChanged();
+                     SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("", Context.MODE_PRIVATE);
+                     HashSet<String> set = new HashSet<>(Memo.notes);
+                     sharedPreferences.edit().putStringSet("notes", set).apply();
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                Memo.notes.set(noteID,String.valueOf(charSequence));
-                Memo.arrayAdapter.notifyDataSetChanged();
-                SharedPreferences sharedPreferences=getApplicationContext().getSharedPreferences(" com.example.memo", Context.MODE_PRIVATE);
-                HashSet<String> set =new HashSet<>(Memo.notes);
-                sharedPreferences.edit().putStringSet("notes",set).apply();
 
-            }
 
-            @Override
-            public void afterTextChanged(Editable editable) {
+             }
 
-            }
-        });
+             @Override
+             public void afterTextChanged(Editable editable) {
+
+             }
+         });
+
     }
-
-    }
-
+}
